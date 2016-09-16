55e53d4939e5ec9551a7eff00afdc4d98def181f
Pascal Birchler <hello@pascalbirchler.ch>
2016-09-16 21:49:55 +0200

    Add two new tests

--- tests/phpunit/tests/test-gp-format-json.php
+++ tests/phpunit/tests/test-gp-format-json.php
@@ -8,4 +8,12 @@ class Test_GP_Format_JSON extends GP_UnitTestCase {
 	public function test_json_format_uses_plugin_class() {
 		$this->assertTrue( GP::$formats['json'] instanceof GP_Format_JSON );
 	}
+
+	public function test_format_name() {
+		$this->assertSame( 'JSON (.json)', GP::$formats['json']->name );
+	}
+
+	public function test_format_extension() {
+		$this->assertSame( 'json', GP::$formats['json']->extension );
+	}
 }
