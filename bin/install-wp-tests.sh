8d044179188a856754a96e32a79f4ad335a2ca71
Pascal Birchler <hello@pascalbirchler.ch>
2016-09-17 11:18:34 +0200

    Update setup script

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -22,22 +22,6 @@ download() {
     fi
 }
 
-if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
-	WP_TESTS_TAG="tags/$WP_VERSION"
-elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-	WP_TESTS_TAG="trunk"
-else
-	# http serves a single offer, whereas https serves multiple. we only want one
-	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
-	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
-	LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-	if [[ -z "$LATEST_VERSION" ]]; then
-		echo "Latest WordPress version could not be found"
-		exit 1
-	fi
-	WP_TESTS_TAG="tags/$LATEST_VERSION"
-fi
-
 set -ex
 
 install_wp() {
@@ -46,27 +30,33 @@ install_wp() {
 		return;
 	fi
 
-	mkdir -p $WP_CORE_DIR
-
-	if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-		mkdir -p /tmp/wordpress-nightly
-		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /tmp/wordpress-nightly/wordpress-nightly.zip
-		unzip -q /tmp/wordpress-nightly/wordpress-nightly.zip -d /tmp/wordpress-nightly/
-		mv /tmp/wordpress-nightly/wordpress/* $WP_CORE_DIR
-	else
-		if [ $WP_VERSION == 'latest' ]; then
-			local ARCHIVE_NAME='latest'
-		else
-			local ARCHIVE_NAME="wordpress-$WP_VERSION"
-		fi
-		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
-		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+	if [ "$( type -t svn )" == '' ]; then
+		echo "install_wp failure: svn is not installed"
+		return 1
 	fi
 
-	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
+	if grep -isqE 'nightly|trunk|alpha|beta|rc' <<< "$WP_VERSION"; then
+		local SVN_URL=https://develop.svn.wordpress.org/trunk/
+	elif [ "$WP_VERSION" == 'latest' ]; then
+		local TAG=$( svn ls https://develop.svn.wordpress.org/tags | tail -n 1 | sed 's:/$::' )
+		local SVN_URL="https://develop.svn.wordpress.org/tags/$TAG/"
+	else
+		local SVN_URL="https://develop.svn.wordpress.org/tags/$WP_VERSION/"
+	fi
+
+	echo "Installing WP from $SVN_URL to $WP_CORE_DIR"
+
+	svn export -q "$SVN_URL" "$WP_CORE_DIR"
+
+	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php "$WP_CORE_DIR/src/wp-content/db.php"
 }
 
 install_gp() {
+	if [ "$( type -t svn )" == '' ]; then
+		echo "install_wp failure: svn is not installed"
+		return 1
+	fi
+
 	# Set up GlotPress
 	echo "Loading GlotPress..."
 	svn export -q https://github.com/glotpress/glotpress-wp/trunk "$WP_CORE_DIR/src/wp-content/plugins/glotpress"
@@ -74,22 +64,14 @@ install_gp() {
 }
 
 install_test_suite() {
-	# set up testing suite if it doesn't yet exist
-	if [ ! -d $WP_TESTS_DIR ]; then
-		# set up testing suite
-		mkdir -p $WP_TESTS_DIR
-		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-	fi
-
 	cd $WP_TESTS_DIR
 
 	if [ ! -f wp-tests-config.php ]; then
-		download https://develop.svn.wordpress.org/${WP_TESTS_TAG}/wp-tests-config-sample.php "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s|localhost|${DB_HOST}|" "$WP_TESTS_DIR"/wp-tests-config.php
+		cp wp-tests-config-sample.php wp-tests-config.php
+		sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
+		sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
+		sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
+		sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
 	fi
 
 	cd - > /dev/null
